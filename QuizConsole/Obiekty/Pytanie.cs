976607986f016b1a9951a4fbb60911b88ff8d80f
@ahermansan <aherman@san.edu.pl>
2022-10-10 13:01:03 +0200

    10.10.2022

---
+++ QuizConsole/Obiekty/Pytanie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizConsole.Obiekty
+{
+    public class Pytanie
+    {
+        public int Id { get; set; }
+        public string Tresc { get; set; }
+        public int Kategoria { get; set; }
+        public string Odp_01 { get; set; }
+        public string Odp_02 { get; set; }
+        public string Odp_03 { get; set; }
+        public string Odp_04 { get; set; }
+
+        public void WyswietlPytanie()
+        {
+            // kod który wuswietla pytanie graczowi
+        }
+
+        public int PodajKategoriePytania()
+        {
+            return Kategoria;
+        }
+
+    }
+}
---
+++ QuizConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using QuizConsole.Obiekty;
+
+Console.WriteLine("WITAJ W QUIZIE");
+var p1 = new Pytanie();
+p1.Id = 1;
+p1.Tresc = "Kiedy wybuchła II wojna światowa?";
+p1.Odp_01 = "1920";
+p1.Odp_02 = "1918";
+p1.Odp_03 = "1939";
+p1.Odp_04 = "1945";
+p1.Kategoria = 100;
+
+
+
